5e0693b1b3f79e7dce7b05d80b58507783b836d4
许灿杰 <xucanjie@4399inc.com>
2017-10-20 11:56:03 +0800

    ：）

--- app/Main.hs
+++ app/Main.hs
@@ -1,99 +1,22 @@
-{-# LANGUAGE DeriveDataTypeable         #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE ApplicativeDo              #-}
 module Main where
-
---import           Control.Monad
-import qualified Prelude hiding(mapM)
-import   Haxl.Prelude
-import           Data.Hashable
-import           Data.Typeable
+import           Data.Pool
+import           Database.MySQL.Simple
+import           Database.MySQL.Simple.Types
 import           Haxl.Core
-import           System.IO
---import Control.Concurrent.STM
-import Control.Concurrent
-type PostId = Int
-type Contents = String
-data BlogRequest a where
-  GetPostIds :: BlogRequest [PostId]
-  GetContents :: PostId -> BlogRequest Contents
-deriving instance Show (BlogRequest a)
-instance ShowP BlogRequest where showp = show
-deriving instance Typeable BlogRequest
-instance Hashable (BlogRequest a) where
-  hashWithSalt salt GetPostIds      = hashWithSalt salt (0::Int)
-  hashWithSalt salt (GetContents x) = hashWithSalt salt (1::Int, x)
-deriving instance Eq (BlogRequest a)
+import           Haxl.Prelude
+import           Lib
+import qualified Prelude                     hiding (mapM)
 
-instance StateKey BlogRequest where
-  data State BlogRequest = BlogDataState
-
-instance DataSourceName BlogRequest where
-  dataSourceName _ = "BlogDataSource"
-
-instance DataSource u BlogRequest where
-  fetch _ _ _ reqs = SyncFetch $ batchFetch reqs
-
-runX :: BlogRequest a -> ResultVar a -> IO ()
-runX GetPostIds var      = putSuccess var [1,2,3,10,12,3,1,23,4,5,6,7,8,9,0]
-runX (GetContents x) var = putSuccess var (show x)
-
-batchContent :: [BlockedFetch BlogRequest] -> ([ResultVar [PostId]],[(PostId,ResultVar Contents)])
-batchContent [] = ([],[])
-batchContent (x:xs) = case x of
-                          BlockedFetch (GetContents v)  var -> (fst pre, (v, var):snd pre)
-                          BlockedFetch GetPostIds var -> (var:fst pre, snd pre)
-  where pre = batchContent xs
-
-runPostids :: [ResultVar [PostId]] -> IO ()
-runPostids xs = forM_ xs $ \i -> putSuccess i [1,2,3,4,5,6,7,8,9,10,11,12,13]
-
-runGetContents :: [(PostId,ResultVar Contents)] -> IO ()
-runGetContents [] = return ()
-runGetContents xs = do
-  print "batchFetch..."
-  print ids --we can batch fetch data here, or we can concrrent fetch data here
-  forM_ (zip ids (map snd xs) ) $ \(i, v) -> putSuccess v i
-  where ids = map (show . fst) xs
-
-batchFetch :: [BlockedFetch BlogRequest] -> IO ()
-batchFetch reqs = do
-  let (bs, as) = batchContent reqs
-  runPostids bs
-  runGetContents as
-
-
-initialState :: StateStore
-initialState = stateSet BlogDataState stateEmpty
-
-getPostIds :: GenHaxl u [PostId]
-getPostIds = dataFetch GetPostIds
-
-getContent :: PostId -> GenHaxl u Contents
---getContent = uncachedRequest . GetContents
-getContent = dataFetch . GetContents
-
-
-haxltest = do
-  postids <- getPostIds
-  a <- mapM getContent postids
-  b <- getContent 10
-  c <- getContent 11
-  return (a, b, c)
+haxltest xs = do
+  indexinfo <- mapM hgetIndexinfo xs
+  userinfo <- mapM hgetUserinfo indexinfo
+  return (zip3 xs indexinfo userinfo)
 
 main :: IO ()
 main = do
-  u <- initEnv initialState ()
-  --r <- runHaxl u getPostIds
-  print "fetch 1"
-  b <- runHaxl u haxltest
-  print "fetch 2 cached"
-  r <- runHaxl u haxltest
-  print "resut:"
-  print (b, r)
+  let conn = defaultConnectInfo
+  pool <- createPool (connect conn) close 10 1000 10
+  let db = (pool, pool)
+  env <- initEnv (stateSet (UdbDataState db) stateEmpty) ()
+  x <- runHaxl env (haxltest (take 10 [1,2..]))
+  print x
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,132 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE ApplicativeDo              #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TypeFamilies               #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import           Control.Concurrent
+import           Control.Concurrent.Async
+import           Control.Exception           hiding (try)
+import           Data.Hashable
+import           Data.List
+import qualified Data.Map                    as Map
+import           Data.Pool
+import           Data.Typeable
+import           Database.MySQL.Simple
+import           Database.MySQL.Simple.Types
+import           Haxl.Core
+import           Haxl.Prelude
+import qualified Prelude                     hiding (mapM)
+import           System.IO
+import           Control.Applicative
+
+type Uid = Int
+type Shardid = Int
+type Idcard = Maybe String
+type Realname = Maybe String
+type Username = String
+type Phone = Maybe String
+type IndexInfo = (Uid, Username, Phone, Shardid)
+type UserInfo = (Uid, Idcard, Realname)
+type DB = (Pool Connection, Pool Connection)
+
+data UdbRequest a where
+  GetIndexInfo :: Uid -> UdbRequest IndexInfo
+  GetUserInfo :: IndexInfo -> UdbRequest UserInfo
+deriving instance Show (UdbRequest a)
+deriving instance Eq (UdbRequest a)
+deriving instance Typeable UdbRequest
+
+instance ShowP UdbRequest where showp = show
+
+instance Hashable (UdbRequest a) where
+  hashWithSalt salt (GetIndexInfo x) = hashWithSalt salt (0::Int, x)
+  hashWithSalt salt (GetUserInfo x)  = hashWithSalt salt (1::Int, x)
+
+instance StateKey UdbRequest where
+  data State UdbRequest = UdbDataState DB
+
+instance DataSourceName UdbRequest where
+  dataSourceName _ = "UdbSource"
+
+instance DataSource u UdbRequest where
+  fetch (UdbDataState db) _ _ reqs = SyncFetch $ batchFetch db reqs
+
+batchFetch :: DB -> [BlockedFetch UdbRequest] -> IO ()
+batchFetch (db1, db2) reqs = do
+  let (as, bs) = splitRequest reqs
+  getIndexinfo db1 as
+  getUserinfo db2 bs
+
+type CO = ([(Uid, ResultVar IndexInfo)], [(IndexInfo, ResultVar UserInfo)])
+splitRequest :: [BlockedFetch UdbRequest] -> CO
+splitRequest [] = ([],[])
+splitRequest (x:xs) = case x of
+                        BlockedFetch (GetIndexInfo uid) var -> ((uid, var):fst pre, snd pre)
+                        BlockedFetch (GetUserInfo i) var -> (fst pre, (i,var):snd pre)
+  where
+    pre = splitRequest xs
+
+class ToMap a where
+  toMap :: [a] -> Map.Map Uid a
+
+instance ToMap IndexInfo where
+  toMap = foldr (\x@(k,_,_,_) acc -> Map.insert k x acc) Map.empty
+
+instance ToMap UserInfo where
+  toMap = foldr (\x@(k,_,_) acc -> Map.insert k x acc) Map.empty
+
+getIndexinfo :: Pool Connection -> [(Uid, ResultVar IndexInfo)] -> IO ()
+getIndexinfo db [] = return ()
+getIndexinfo db xs = do
+  print "BatchFecht: " >> print (length uids)
+  res::(Map.Map Uid IndexInfo) <- fmap toMap $ withResource db $ \s -> query s (fromString q) $ Only (In uids)
+  forM_ xs $ \(i, v) -> do
+    let r = Map.lookup i res
+    case r of
+      Nothing -> putSuccess v (0, [], Nothing, 0)
+      Just c  -> putSuccess v c
+  where
+    uids = map fst xs :: [Int]
+    q = "select uid,username,bindedphone,shardid from ui_index where uid in ?"
+
+
+hgetIndexinfo :: Uid -> GenHaxl u IndexInfo
+hgetIndexinfo = dataFetch . GetIndexInfo
+
+hgetUserinfo :: IndexInfo -> GenHaxl u UserInfo
+hgetUserinfo = dataFetch . GetUserInfo
+
+groupQuery :: [IndexInfo] -> Map.Map Shardid [Uid]
+groupQuery [] = Map.empty
+groupQuery (x@(u,_,_,s):xs) = case ispre of
+                                Nothing -> Map.insert s [u] pre
+                                Just uids -> Map.update (\a -> Just (u:a)) s pre
+  where
+    pre = groupQuery xs
+    ispre = Map.lookup s pre
+
+getUserinfo :: Pool Connection -> [(IndexInfo, ResultVar UserInfo)] -> IO ()
+getUserinfo db [] = return ()
+getUserinfo db xs = do
+  res <-  (toMap . concat) Control.Applicative.<$>  mapConcurrently doGetone r
+  forM_ xs $ \((u,_,_,_),v) -> case Map.lookup u res of
+                                 Nothing -> putSuccess v (0, Nothing, Nothing)
+                                 Just r -> putSuccess v r
+  where
+    r = Map.toList . groupQuery $ filter (\(u,_,_,_) -> u /= 0 ) $ map fst xs
+    doGetone :: (Shardid, [Uid]) -> IO [UserInfo]
+    doGetone (x, uids) =
+      withResource db $ \p -> query p (fromString q) $ Only (In uids)
+      where
+        q =  "select uid,idcard, realname from user_info_" ++ table ++ " where uid in ?"
+        table
+          | x < 10 = "00" ++ show x
+          | otherwise = "0" ++ show x
